ce6a476dd473734887d03bfcf5a8699a821fbc20
amirhmi <amirhmi1377@gmail.com>
2019-03-23 00:40:21 +0430

    project get data from api

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { ErrorHandlerService } from 'src/core/error-handler-service';
-import User from 'src/views/user/user';
+import axios from 'axios';
 import './home.scss';
 import Header from 'src/views/layout/header/header'
 import Footer from 'src/views/layout/footer/footer'
@@ -8,51 +7,28 @@ import Main from 'src/views/layout/main/main'
 
 
 export default class Home extends Component<Props, State> {
-  getUsers() {
-    fetch(new Request('http://localhost:8001/posts/1', { method: 'GET' }))
-      .then(response => {
-        if (!response.ok) ErrorHandlerService(response);
-        return response.json();
-      })
-      .then(response => {
-        console.log('TCL: Home -> getUsers -> response', response);
-      })
-      .catch(error => {
-        console.log('TCL: Home -> getUsers -> error', error);
-        ErrorHandlerService(error);
-      });
-  }
-  handlePnClick: (inputValue: string) => void = inputValue => {
-    console.log('TCL: Home -> handlePnClick: -> inputValue', inputValue);
-  };
-
-  addSkill: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void = event => {
-    this.setState({ skills: [...this.state.skills, { name: 'js', endorse: 333 }] });
-  };
-  removeSkill: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void = event => {
-    this.setState({ skills: this.state.skills.filter(s => s.name === 'js') });
-  };
+  
   constructor(props: Props) {
     super(props);
     this.state = {
-      skills: [{ name: 'HTML', endorse: 4 }, { name: 'CSS', endorse: 5 }]
-    };
+      projectId: "20955d46-0aac-4a6a-b546-d1581026663f",
+    }
   }
-  componentDidMount = () => {
-    this.getUsers();
-  };
-  componentWillUnmount = () => {};
+
+  componentDidMount() {
+    this.getProject();
+  }
+
+  getProject = () => {
+    axios.get(`http://localhost:8080/projects/` + this.state.projectId)
+      .then( (res: any) => {
+        this.setState({
+          project: res.data
+        });
+      });
+  }
 
   render() {
-    const { skills } = this.state;
-
-    const skillsComp = skills.map((s, i) => (
-      <div key={i}>
-        <span>{s.name}</span>
-        <span>{s.endorse}</span>
-      </div>
-    ));
-
     return (
       <body>
         <Header />
@@ -61,18 +37,25 @@ export default class Home extends Component<Props, State> {
       </body>
     );
   }
-  // onClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-  //   if (event.ctrlKey) {
-  //     this.setState((state, props) => ({ title: `${state.title} - new title` }));
-  //   }
-  // }
+  
 }
 
-interface State {
+interface Project {
+  budget: number;
+  deadline: number;
+  description: string;
+  id: string
+  imageUrl: string;
   skills: Skill[];
+  title: string;
+  winner: string;
+}
+interface State {
+  project?: Project,
+  projectId: string
 }
 interface Skill {
   name: string;
-  endorse: number;
+  point: number;
 }
 interface Props {}
