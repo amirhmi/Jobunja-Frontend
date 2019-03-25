273898d9ed110577eaf1120dc2d2130de7935020
amirhmi <amirhmi1377@gmail.com>
2019-03-25 16:20:06 +0430

    home page link in header added

--- src/views/layout/header/header.tsx
+++ src/views/layout/header/header.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './header.scss'
 import JobunjaLogo from '../../../recourse/logo/logo-v1.png'
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component<Props, State> {
   //TODO: correct <a> links
@@ -12,9 +13,9 @@ export default class Header extends Component<Props, State> {
                     <div className="row navbar-header align-items-center">
                         <div className="col-1">
                             <span className="logo_icon">
-                                <a>
+                                <Link to="/">
                                     <img src={JobunjaLogo} alt="logo" />
-                                </a>
+                                </Link>
                             </span>
                         </div>
                         <div className="col-2 offset-8 account-link">
